0c478fbf1fee9cbba1d779a01aed5f5d030bfafe
BakerAO <dpshunter@gmail.com>
2018-02-07 12:18:40 -0600

    Added drum functionality
    Signed-off-by: BakerAO <dpshunter@gmail.com>

--- 2-beat-mix/public/js/script.js
+++ 2-beat-mix/public/js/script.js
@@ -5,4 +5,34 @@ let snares = makeFalse();
 let hiHats = makeFalse();
 let rideCymbals = makeFalse();
 
-console.log(kicks);
+const getDrumArray = function (drumName) {
+    switch (drumName){
+        case 'kicks':
+            return kicks;
+        case 'snares':
+            return snares;
+        case 'hiHats':
+            return hiHats;
+        case 'rideCymbals':
+            return rideCymbals;
+        default:
+            return;
+    }
+};
+
+const toggleDrum = function(drumName, index){
+    const drumArray = getDrumArray(drumName);
+    drumArray[index] = !drumArray[index];
+};
+
+const clear = function (drumName){
+    const drumArray = getDrumArray(drumName);
+    drumArray.fill(false);
+};
+
+const invert = function(drumName){
+    const drumArray = getDrumArray(drumName);
+    for(let i = 0; i < drumArray.length; i++){
+        drumArray[i] = !drumArray[i];
+    }
+};
